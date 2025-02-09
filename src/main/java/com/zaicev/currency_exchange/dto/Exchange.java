6c836a502bdcf1a19a5108452c82b46aaefec356
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-02-09 18:20:45 +0600

    Создан DTO для результатов обмена

---
+++ src/main/java/com/zaicev/currency_exchange/dto/Exchange.java
@@ -0,0 +1,22 @@
+package com.zaicev.currency_exchange.dto;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.zaicev.currency_exchange.model.Currency;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@JsonPropertyOrder({"baseCurrency", "targetCurrence", "rate", "amount", "convertedAmount"})
+public class Exchange {
+	private Currency baseCurrency;
+	private Currency targetCurrency;
+	private BigDecimal rate;
+	private BigDecimal amount;
+	private BigDecimal convertedAmount;
+}
