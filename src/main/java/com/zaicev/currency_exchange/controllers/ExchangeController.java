c4792de2f9a45325b72b95195f6ded14247b8466
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-02-09 21:43:59 +0600

    добавлен реверсивный метод и транзитный через usd получение курса

--- src/main/java/com/zaicev/currency_exchange/controllers/ExchangeController.java
+++ src/main/java/com/zaicev/currency_exchange/controllers/ExchangeController.java
@@ -1,6 +1,8 @@
 package com.zaicev.currency_exchange.controllers;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Optional;
 
 import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.zaicev.currency_exchange.dto.Exchange;
+import com.zaicev.currency_exchange.model.Currency;
 import com.zaicev.currency_exchange.model.ExchangeRate;
+import com.zaicev.currency_exchange.repository.CurrencyRepository;
 import com.zaicev.currency_exchange.repository.ExchangeRateRepository;
 
 import jakarta.validation.constraints.NotNull;
@@ -24,13 +28,41 @@ public class ExchangeController {
 	@Autowired
 	ExchangeRateRepository exchangeRateRepository;
 
+	@Autowired
+	CurrencyRepository currencyRepository;
+
 	@GetMapping()
 	public Exchange getExchange(@RequestParam @Length(min = 3, max = 3) String from,
 			@RequestParam @Length(min = 3, max = 3) String to, @RequestParam @NotNull BigDecimal amount) {
-		ExchangeRate exchangeRate = exchangeRateRepository.findByBaseAndTargetCurrencyCode(from, to)
-				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
-		return new Exchange(exchangeRate.getBaseCurrency(), exchangeRate.getTargetCurrency(), exchangeRate.getRate(),
-				amount, amount.multiply(exchangeRate.getRate()));
+		Currency baseCurrency = currencyRepository.findByCode(from).orElseThrow(
+				() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Не найдена валюта с кодом " + from));
+		Currency targetCurrency = currencyRepository.findByCode(to).orElseThrow(
+				() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Не найдена валюта с кодом " + to));
+
+		BigDecimal rate = GetRate(from, to);
+
+		return new Exchange(baseCurrency, targetCurrency, rate, amount, amount.multiply(rate).setScale(2, RoundingMode.DOWN));
+	}
+
+	private BigDecimal GetRate(String from, String to) {
+		Optional<ExchangeRate> exchangeRate = exchangeRateRepository.findByBaseAndTargetCurrencyCode(from, to);
+		if (exchangeRate.isPresent()) {
+			return exchangeRate.get().getRate();
+		}
+
+		Optional<ExchangeRate> reverseExchangeRate = exchangeRateRepository.findByBaseAndTargetCurrencyCode(to, from);
+		if (reverseExchangeRate.isPresent()) {
+			return (new BigDecimal(1)).divide(reverseExchangeRate.get().getRate(), 6, RoundingMode.HALF_UP);
+		}
+
+		Optional<ExchangeRate> exchangeRateUsdFrom = exchangeRateRepository.findByBaseAndTargetCurrencyCode("USD",
+				from);
+		Optional<ExchangeRate> exchangeRateUsdTo = exchangeRateRepository.findByBaseAndTargetCurrencyCode("USD", to);
+		if (exchangeRateUsdFrom.isPresent() && exchangeRateUsdTo.isPresent()) {
+			return exchangeRateUsdTo.get().getRate().divide(exchangeRateUsdFrom.get().getRate(), 6, RoundingMode.HALF_UP);
+		}
+
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Не найден курс обмена");
 	}
 }
